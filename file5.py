3ea2ad901a5738827f5ba65c8ab8afdabce29c6d
Sundar <ext-sbariga@exo.inc>
2023-09-05 23:25:29 +0530

    file5-3

--- file5.py
+++ file5.py
@@ -2,6 +2,8 @@
 num = int(input("Enter a number: "))
 num = int(input("Enter a number: "))
 num = int(input("Enter a number: "))
+num = int(input("Enter a number: "))
+
 
 if num % 2 == 0:
     print(f"{num} is even.")
